896d7493d5aa1fabceb4615e48349f8ec9fd1e13
Juozas <juozas@ivejas.lt>
2021-01-15 11:30:33 +0200

    jsx vs create element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+```// <div className="App">
+    //  <p>abc</p>
+    // </div>```
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const p =    React.createElement('p', {}, 'Abc')
+  const div = React.createElement('div', { className: 'App'}, 
+p
+  );``
+  return  div
 }
 
 export default App;
