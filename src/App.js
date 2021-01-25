a1444a371180dcc4ee59ac2bcb8cdc7953ca3fe5
juozasm <31696919+juozasm@users.noreply.github.com>
2021-01-25 16:39:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 
-```// <div className="App">
-    //  <p>abc</p>
-    // </div>```
+// <div className="App">
+//  <p>abc</p>
+// </div>
+
+// UPDATE: This is also valid, but must be set as UPPERCASE var
+/* 
+    const CustomTag = `h${this.props.priority}`;
+    <CustomTag>Hello</CustomTag>
+*/
+
+
 
 function App() {
   const p =    React.createElement('p', {}, 'Abc')
